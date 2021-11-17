a668d20cb97fdb2c4f81d524adc2bb7fa67d7985
Egil <egil.moller@freecode.no>
2021-11-17 11:58:02 +0100

    Ability to emulate standard confusion matrix

--- skl_emeralds/confusion_matrix.py
+++ skl_emeralds/confusion_matrix.py
@@ -1,6 +1,7 @@
 import seaborn as sn
 
 def plot_confusion_matrix(model, features, labels, label_names,
+                          count=False,
                           cmap="viridis", norm = matplotlib.colors.LogNorm(),
                           ax = None,
                           **kw):
@@ -10,17 +11,28 @@ def plot_confusion_matrix(model, features, labels, label_names,
     proba_layer = model.predict_proba(features)
     label_layer = model.classes_[np.argmax(proba_layer, axis=1)]
 
+    if count:
+        m = proba_layer.argmax(axis=1)
+        p = np.zeros(proba_layer.shape).flatten()
+        p[np.ravel_multi_index((np.arange(len(m)), m), proba_layer.shape)] = 1
+        p = p.reshape(*proba_layer.shape)
+        proba_layer = p
+    
     res = np.zeros((np.max(labels)+1, np.max(labels) + 1))
     for label in np.unique(labels):
         res[label,model.classes_] = proba_layer[labels == label, :].sum(axis=0)
 
-    rowsum = res.sum(axis=1)
-    rowsum = np.where(rowsum == 0, 1, rowsum)
-    res = res / numpy.tile(np.array([rowsum]).transpose(), (1, res.shape[1]))
-
+    if not count:
+        rowsum = res.sum(axis=1)
+        rowsum = np.where(rowsum == 0, 1, rowsum)
+        res = 100 * res / numpy.tile(np.array([rowsum]).transpose(), (1, res.shape[1]))
+        format = ".1f"
+    else:
+        format = ".0f"
+    
     label_names_by_label = label_names.reset_index().set_index(0)["index"]
-
-    sn.heatmap(res, annot=True, annot_kws={"size": 16}, fmt=".3f",
+    
+    sn.heatmap(res, annot=True, annot_kws={"size": 16}, fmt=format,
                xticklabels = label_names_by_label,
                yticklabels = label_names_by_label,
                norm = norm,
