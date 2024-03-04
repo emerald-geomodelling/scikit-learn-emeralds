91c9fb2af39b444d9ca36e698a52e206d9f15236
Duke-of-Lizard <cch@emeraldgeo.no>
2024-03-04 15:32:14 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ import os
 
 setuptools.setup(
     name='scikit-learn-emeralds',
-    version='0.0.2',
+    version='0.0.3',
     description='Collection of utils for scikit-learn & scipy',
     long_description="""Collection of utils for scikit-learn & scipy""",
     long_description_content_type="text/markdown",
-    author='Egil Moeller',
+    author='Egil Moeller, Craig W. Christensen, et al.',
     author_email='em@emeraldgeo.no',
     url='https://github.com/emerald-geomodelling/scikit-learn-emeralds',
     packages=setuptools.find_packages(),
